a0e65a327cd14de5584621ec244a908e7754872a
Ivan Pustogarov <ip99@cornell.edu>
2016-12-05 15:22:24 -0500

    Remvoe torlib/pyublas
    This was moved to xorcpp

--- torlib/pyublas/test/sample.py
+++
@@ -1,7 +0,0 @@
-import numpy
-import sample_ext
-import pyublas # not explicitly used--but makes converters available
-
-vec = numpy.ones((5,), dtype=float)
-print vec
-print sample_ext.doublify(vec)
